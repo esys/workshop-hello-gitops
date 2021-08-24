066a7ed30d1a50e409d57a0282832e87b3b2595e
pkatira <piyush.katira@coupa.com>
2021-08-24 15:39:24 +0400

    changes to app name

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		log.Printf("unknown host")
 	}
-	resp := fmt.Sprintf("Hello to PHK in Development branch",)
+	resp := fmt.Sprintf("Hello to PHK in Development branch ONLY",)
 
 	_, err = w.Write([]byte(resp))
 	if err != nil {
