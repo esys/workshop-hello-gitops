10431365fa73ae65d7bf970ffed7579d944d776c
pkatira <piyush.katira@coupa.com>
2021-08-24 16:43:24 +0400

    change in message

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		log.Printf("unknown host")
 	}
-	resp := fmt.Sprintf("Hello to PHK in Development branch ONLY",)
+	resp := fmt.Sprintf("Hello to VMware folks in Development branch",)
 
 	_, err = w.Write([]byte(resp))
 	if err != nil {
