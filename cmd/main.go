a8a901e230c3a4a463bee3d25898ae3bfd631b0c
pkatira <piyush.katira@coupa.com>
2021-08-24 09:44:50 +0400

    message change in master branch

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		log.Printf("unknown host")
 	}
-	resp := fmt.Sprintf("Hello from Piyush in Development branch",)
+	resp := fmt.Sprintf("Hello from Piyush in Master branch",)
 	_, err = w.Write([]byte(resp))
 	if err != nil {
 		log.Panicf("not able to write http output: %s", err)
