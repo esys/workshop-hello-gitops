a2d533e6a350bd10ed843283d25a7941332c63cb
pkatira <piyush.katira@coupa.com>
2021-09-01 11:28:41 +0400

    made change to the pod message

--- cmd/main.go
+++ cmd/main.go
@@ -25,8 +25,12 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		log.Printf("unknown host")
 	}
+<<<<<<< Updated upstream
 	resp := fmt.Sprintf("Hello  to Salvatore and Zahoor from Piyush!",)
 
+=======
+	resp := fmt.Sprintf("Hello  to awesome VMware Tanzu team from Piyush!",)
+>>>>>>> Stashed changes
 	_, err = w.Write([]byte(resp))
 	if err != nil {
 		log.Panicf("not able to write http output: %s", err)
