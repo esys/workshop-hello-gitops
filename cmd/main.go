1833703b312202ebd7b2d4ec6e41dafd2e570df2
pkatira <piyush.katira@coupa.com>
2021-09-01 10:08:41 +0400

    made changes to the pod messaging

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		log.Printf("unknown host")
 	}
-	resp := fmt.Sprintf("Hello from Piyush!",)
+	resp := fmt.Sprintf("Hello from Piyush to Salvatore and Zahoor!",)
 
 	_, err = w.Write([]byte(resp))
 	if err != nil {
