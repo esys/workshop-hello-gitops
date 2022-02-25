19e3e99282de8a6b13065f90d47bdc521c784d48
erik <erikkim@erikui-MacBookPro.local>
2022-02-25 11:14:40 +0900

    customized settings -- 9

--- cmd/main.go
+++ cmd/main.go
@@ -13,19 +13,19 @@ func main() {
 	startServer(handler)
 }
 
-func startServer(handler func(http.ResponseWriter, *http.Request)){
+func startServer(handler func(http.ResponseWriter, *http.Request)) {
 	http.HandleFunc("/", handler)
 	log.Printf("starting server...")
 	http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
+func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("received request from %s", r.Header.Get("User-Agent"))
 	host, err := os.Hostname()
 	if err != nil {
 		host = "unknown host"
 	}
-	resp := fmt.Sprintf("Hello from %s", host)
+	resp := fmt.Sprintf("Hello Erik from %s", host)
 	_, err = w.Write([]byte(resp))
 	if err != nil {
 		log.Panicf("not able to write http output: %s", err)
